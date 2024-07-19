022475bd7019ff1838de5440e8cee24be38db5ee
aryanbuzz9 <aryanbuzz9@gmail.com>
2024-07-19 09:01:00 +0530

    Added Code for find maximum sum

--- demo.py
+++ demo.py
@@ -1,5 +1,14 @@
-import pandas
-
+# import pandas
+a=[100,200,300,400]
+n=len(a)
+k=2
+def sub_array(a,k):
+    window_sum=sum(a[:k])
+    for i in range(n-k):
+        current_sum=window_sum-a[i]+a[k+i]
+        window_sum=max(window_sum,current_sum)
+    return window_sum
+print(sub_array(a,k))
 
 
 
