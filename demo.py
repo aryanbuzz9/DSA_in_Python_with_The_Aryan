b11efb1e92c05018cf7a8412f062775981d3db69
aryanbuzz9 <aryanbuzz9@gmail.com>
2024-07-19 09:26:47 +0530

    Added subarray also

--- demo.py
+++ demo.py
@@ -8,9 +8,21 @@ def sub_array(a,k):
         current_sum=window_sum-a[i]+a[k+i]
         window_sum=max(window_sum,current_sum)
     return window_sum
+# print(sub_array(a,k))
+
+#For printing the subarray having maximum sum
+
+l=a[:]
+def sub_array(a,k):
+    window_sum=sum(a[:k])
+    for i in range(n-k):
+        current_sum=window_sum-a[i]+a[i+k]
+        if(window_sum<=current_sum):
+            window_sum=current_sum
+            l.remove(a[i])
+    return window_sum,l
+
 print(sub_array(a,k))
-
-
 
 
 
